d23c8f963fbe89401cffd2f2cfca26a78a978fb0
Tokomichel <tokowouendeu@gmail.com>
2025-11-07 09:43:00 +0100

    First commit: initializing files

---
+++ app.js
@@ -0,0 +1,14 @@
+function changeHoverImage() {
+    const image = "myself.jpg";
+    const profileImage = document.querySelector('.profile-image img');
+    profileImage.src = image;
+}
+
+function changeNormalImage() {
+    const image = "me.jpg";
+    const profileImage = document.querySelector('.profile-image img');
+    profileImage.src = image;
+}
+
+
+
